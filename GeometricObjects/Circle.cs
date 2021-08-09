e17765edac9dd9ba575036858071665d5f0bf371
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-09 13:22:08 +0200

    Get und Set

--- GeometricObjects/Circle.cs
+++ GeometricObjects/Circle.cs
@@ -14,8 +14,41 @@ namespace GeometricObjects
     /// </remarks>
     public class Circle
     {
-        public double XCoordinate;
-        public double YCoordinate;
-        public int radius;
+        //Eigenschaften > Auf public um Sie zu Kapseln
+        private double XCoordinate;
+        private double YCoordinate;
+        private int Radius;
+
+        //Get
+        public double getX()
+        {
+            return XCoordinate;
+        }
+
+        public double getY()
+        {
+            return YCoordinate;
+        }
+
+        public double getRad()
+        {
+            return Radius;
+        }
+
+        //set
+        public void setX(double position)
+        {
+            XCoordinate = position;
+        }
+
+        public void setY(double position)
+        {
+            YCoordinate = position;
+        }
+
+        public void setRad(int rad)
+        {
+            Radius = rad;
+        }
     }
 }
