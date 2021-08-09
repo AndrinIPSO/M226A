21b420e638fbbfd3bf43f16dfc8692d5874837cc
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-09 13:13:59 +0200

    Add project files.

---
+++ GeometricObjects/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    /// <summary>
+    /// Kreisklasse
+    /// </summary>
+    /// <remarks>
+    /// Übung Kreisklasse vom Buch
+    /// </remarks>
+    public class Circle
+    {
+        public double XCoordinate;
+        public double YCoordinate;
+        public int radius;
+    }
+}
