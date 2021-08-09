168719c1c47428b3fd145204c53b0d3c74698ee7
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-09 16:44:29 +0200

    Mathematik Klasse

--- GeometricObjects/Program.cs
+++ GeometricObjects/Program.cs
@@ -23,6 +23,20 @@ namespace GeometricObjects
             }
             //Console.WriteLine($"Fläche = {C1.getArea()}");
             //Console.WriteLine("Umfang = {0}", C1.getCircumference());
+
+            try
+            {
+                Mathematik.Addition(1, 2);
+                Mathematik.Subtraktion(2, 1);
+                Mathematik.Multiplikation(3, 4);
+                Mathematik.Division(10, 2);
+                Mathematik.Modulo(10, 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+
+            }
             Console.ReadLine();
 
 
