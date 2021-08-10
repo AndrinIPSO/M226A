7724683c44419363e78610b874a49ea549050034
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-10 15:52:10 +0200

    Account und Addressbook mit List und Protected

---
+++ Adressbuch/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbuch
+{
+    class Account
+    {
+
+        protected string _emailAdress;
+        protected string _name;
+
+
+        public string EmailAddress
+        {
+            get { return _emailAdress; }
+            set { _emailAdress = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        public List<Addressbook> Uses { get; set; }
+
+    }
+}
