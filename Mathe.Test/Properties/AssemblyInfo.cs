0de77327666bae5c504cb9e0785eb1f8b5a494cc
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-17 14:36:56 +0200

    Unit Testing und Mathematik namespace

---
+++ Mathe.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mathe.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mathe.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5672b911-0c9a-4ad8-aca0-8cb37d3ca4db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mathe.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Mathe;
+
+namespace Mathe.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            int a = 20;
+            int b = 5;
+            int result = 0;
+
+            result = Mathematik.Addition(a, b);
+            Assert.AreEqual(13, result, "Ist falsch", a, b);
+            result = Mathematik.Division(a, b);
+            Assert.AreEqual(4, "Verkackt");
+
+
+        }
+        [TestMethod]
+
+        public void Kuletest()
+        {
+            int a = 20;
+            int b = 5;
+            int result = 0;
+
+            result = Mathematik.Division(a, b);
+            Assert.AreEqual(4, result, "Ist falsch", a, b);
+
+
+        }
+    }
+}
